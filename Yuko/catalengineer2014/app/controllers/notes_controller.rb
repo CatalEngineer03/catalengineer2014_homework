7f65ec92198ed6d30287706310d3d781a69e774f
yuko210 <yuko@Yukos-MacBook-Air.local>
2014-10-30 20:16:16 +0900

    更新削除宿題

--- Yuko/catalengineer2014/app/controllers/notes_controller.rb
+++ Yuko/catalengineer2014/app/controllers/notes_controller.rb
@@ -36,4 +36,11 @@ class NotesController < ApplicationController
        render :edit
      end
    end
+
+   def destroy
+     @note = Note.find(params[:id])
+     @note.destroy
+     redirect_to notes_path
+   end
+
 end
