449c8006920f869d743033660891cf0659a2041c
Sachiko Sakamoto <sakamotosa25@gmail.com>
2014-11-06 20:51:05 +0900

    group & profile

---
+++ sachiko/catalengineer2014/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Catalengineer2014::Application.config.secret_key_base = 'b40ef6c0aff3a3b2655edc57a5735a66bb7d923dbeb406155da89e5f219a1cb166e3f2434ec9f30e5a70ff40890c2ee45ce3db324d042ebd8ae615913047a5fa'
